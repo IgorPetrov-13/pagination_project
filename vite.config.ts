d4b1ad073369e0c28b55c071ed6734effe3b1d1f
IgorPetrov-13 <petrov_igor_rf@mail.ru>
2025-03-31 22:54:53 +0300

    fix: Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: '/pagination_project/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
@@ -16,14 +16,4 @@ export default defineConfig({
       },
     },
   },
-
-  //!
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:3000/',
-  //       changeOrigin: true,
-  //     },
-  //   },
-  // }, 
 });
